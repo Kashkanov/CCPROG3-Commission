791636afc5d9cac6264be811a1a49b9c7334d293
superkrunchii <josh_chua@dlsu.edu.ph>
2022-06-24 22:53:24 +0800

    Added edit

--- src/manager/main_Menu.java
+++ src/manager/main_Menu.java
@@ -1,4 +1,5 @@
 package manager;
+
 import records.Patient;
 import service.Service;
 
@@ -7,7 +8,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.ArrayList;
-
 
 public class main_Menu {
 
@@ -24,12 +24,12 @@ public class main_Menu {
 
 		main_Menu menu = new main_Menu();
 
-		if(f1.exists())
+		if (f1.exists())
 			menu.readPatients(patients);
-		if(f2.exists())
+		if (f2.exists())
 			menu.readServices(services);
 
-		//menu.printPatients(patients);
+		// menu.printPatients(patients);
 		records.ManagePatientRecords record = new records.ManagePatientRecords();
 		service.ManageServices service = new service.ManageServices();
 
@@ -56,12 +56,12 @@ public class main_Menu {
 							record.ProcessPatientRecord(patients, 3);
 							break;
 						case "4":
-							//Search
-							//try {
-								record.ProcessPatientRecord(patients, 4);
-							//} catch (FileNotFoundException e) {
-							//	e.printStackTrace();
-							//}
+							// Search
+							// try {
+							record.ProcessPatientRecord(patients, 4);
+							// } catch (FileNotFoundException e) {
+							// e.printStackTrace();
+							// }
 							break;
 						case "X":
 							main_Menu.main(null);
@@ -69,29 +69,29 @@ public class main_Menu {
 					}
 					break;
 				case 2:
-				menu.displayManageServices();
-				variable = sc.nextLine();
-				switch (variable) {
-					case "1":
-						// Add
-						service.AddNewService(services);
+					menu.displayManageServices();
+					variable = sc.nextLine();
+					switch (variable) {
+						case "1":
+							// Add
+							service.AddNewService(services);
 
-						break;
-					case "2":
-						service.ProcessService(services, 2);
-					
-					case "3":
-						// Delete
-						service.ProcessService(services, 3);
-						break;
-					case "4":
-						// Edit
-					
-						break;
-					case "X":
-						main_Menu.main(null);
-						break;
-				}
+							break;
+						case "2":
+							service.ProcessService(services, 2);
+
+						case "3":
+							// Delete
+							service.ProcessService(services, 3);
+							break;
+						case "4":
+							// Edit
+							service.ProcessService(services, 4);
+							break;
+						case "X":
+							main_Menu.main(null);
+							break;
+					}
 					break;
 				case 3:
 					System.out.println("Manage Laboratory Results");
@@ -130,37 +130,41 @@ public class main_Menu {
 		System.out.println("[1] Add New Service");
 		System.out.println("[2] Search Service");
 		System.out.println("[3] Delete Service");
-		System.out.println("[4] Search Edit Service");
+		System.out.println("[4] Edit Service");
 		System.out.println("[X] Return to Main Menu\n");
 		System.out.print("Select a transaction: ");
 	}
 
-	/*puts all patient data from patients.txt excluding the deleted patients
+	/*
+	 * puts all patient data from patients.txt excluding the deleted patients
 	 * 
 	 */
-	public void readPatients(ArrayList<Patient> patients){
+	public void readPatients(ArrayList<Patient> patients) {
 		String filepath = "Patients.txt";
 
 		try {
 			Scanner x = new Scanner(new File(filepath));
-			//x.useDelimiter(";");
-			System.out.println(x);
+			// x.useDelimiter(";");
 
 			while (x.hasNext()) {
 				String fullString = x.nextLine();
-				
+
 				String[] splitString = fullString.split(";");
-					
-					//System.out.println("deleted");	//<===
-				if(splitString.length <= 9 ){
-					//System.out.println(fullString);	//<===
-					patients.add(new Patient(fullString, splitString[0], splitString[1], splitString[2], splitString[3], Long.parseLong(splitString[4]), splitString[5].charAt(0), splitString[6], splitString[7], splitString[8]));
-				}else{
-					//System.out.println("reason = );	//<===
-					patients.add(new Patient(fullString, splitString[0], splitString[1], splitString[2], splitString[3], Long.parseLong(splitString[4]), splitString[5].charAt(0), splitString[6], splitString[7], splitString[8], splitString[10]));
-					
+
+				// System.out.println("deleted"); //<===
+				if (splitString.length <= 9) {
+					// System.out.println(fullString); //<===
+					patients.add(new Patient(fullString, splitString[0], splitString[1], splitString[2], splitString[3],
+							Long.parseLong(splitString[4]), splitString[5].charAt(0), splitString[6], splitString[7],
+							splitString[8]));
+				} else {
+					// System.out.println("reason = ); //<===
+					patients.add(new Patient(fullString, splitString[0], splitString[1], splitString[2], splitString[3],
+							Long.parseLong(splitString[4]), splitString[5].charAt(0), splitString[6], splitString[7],
+							splitString[8], splitString[10]));
+
 				}
-					
+
 			}
 
 		} catch (Exception e) {
@@ -168,38 +172,38 @@ public class main_Menu {
 		}
 
 	}
-	
-	//for testing
-	public void printPatients(ArrayList<Patient> patients){
+
+	// for testing
+	public void printPatients(ArrayList<Patient> patients) {
 		System.out.println("Patients:");
-		for(int i=0; i<patients.size();i++){
+		for (int i = 0; i < patients.size(); i++) {
 			System.out.println(patients.get(i).getFullString());
 		}
 	}
 
-	public void readServices(ArrayList<Service> services){
+	public void readServices(ArrayList<Service> services) {
 		String filepath = "Services.txt";
 
 		try {
 			Scanner x = new Scanner(new File(filepath));
-			//x.useDelimiter(";");
-			System.out.println(x);
+			// x.useDelimiter(";");
 
 			while (x.hasNext()) {
 				String fullString = x.nextLine();
-				
+
 				String[] splitString = fullString.split(";");
-					
-				
-				if(splitString.length > 3 ){
-					//System.out.println(fullString);	//<===
-					services.add(new Service(fullString, splitString[0], splitString[1], Integer.parseInt(splitString[2]), splitString[3]));
-				}else{
-					//System.out.println("reason = );	//<===
-					services.add(new Service(fullString, splitString[0], splitString[1], Integer.parseInt(splitString[2])));
-					
+
+				if (splitString.length > 3) {
+					// System.out.println(fullString); //<===
+					services.add(new Service(fullString, splitString[0], splitString[1],
+							Integer.parseInt(splitString[2]), splitString[3]));
+				} else {
+					// System.out.println("reason = ); //<===
+					services.add(
+							new Service(fullString, splitString[0], splitString[1], Integer.parseInt(splitString[2])));
+
 				}
-					
+
 			}
 
 		} catch (Exception e) {
