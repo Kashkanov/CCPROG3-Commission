dd3dbfb9b2f8be4ee247e5f2aec0a650bc21ac40
superkrunchii <josh_chua@dlsu.edu.ph>
2022-06-25 12:12:37 +0800

    Fixed condition removing deleted records from matches

--- src/service/SearchService.java
+++ src/service/SearchService.java
@@ -79,7 +79,7 @@ public class SearchService {
 			for (int i = 0; i < services.size(); i++) {
 				// System.out.println(key + "==" + services.get(i).getServCode() + "//" +
 				// key.equals(services.get(i).getServCode()));//<===
-				if (key.equals(services.get(i).getServCode())) {
+				if (key.equals(services.get(i).getServCode()) && services.get(i).deleted != 'D') {
 					matchesInd.add(i);
 				}
 			}
@@ -87,7 +87,7 @@ public class SearchService {
 			for (int i = 0; i < services.size(); i++) {
 				// System.out.println(key + "==" + services.get(i).getDescription() + "//" +
 				// key.contains(services.get(i).getDescription()));//<===
-				if (services.get(i).getDescription().contains(key)) {
+				if (services.get(i).getDescription().contains(key) && services.get(i).deleted != 'D') {
 					matchesInd.add(i);
 				}
 			}
