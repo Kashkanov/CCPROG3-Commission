43ecc9e77cedcfed1a4674c708d7d505edd5ae83
superkrunchii <josh_chua@dlsu.edu.ph>
2022-06-26 08:59:04 +0800

    Removed unnecessary methods

--- src/records/AddNewPatient.java
+++ src/records/AddNewPatient.java
@@ -2,24 +2,21 @@ package records;
 
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class AddNewPatient {
 	// Properties
 	private static String month = "05"; // For TEST
-	private int k = 0;
 	private ArrayList<String> list = new ArrayList<>();
 
 	// Methods
 	public void SaveRecord(Patient patient) {
-		//this.list.addAll(list2);
+		// this.list.addAll(list2);
 		try {
 			// Creates a new Text file called Patients.txt
 			File myObj = new File("Patients.txt");
@@ -44,25 +41,26 @@ public class AddNewPatient {
 
 			pw.print(patient.getFullString() + ";");
 			/*
-			for (int i = 0; i < this.list.size(); i++) {
-				removeDuplicate(this.list);
-				pw.print(this.list.get(i) + ";");
-				k++;
-				if (k == 9) {
-					pw.println();
-					k = 0;
-				}
-			}
-			
-			pw.print(patient.getUID() + ";");
-			pw.print(patient.getFirstName() + ";");
-			pw.print(patient.getlastName() + ";");
-			pw.print(patient.getMiddleName() + ";");
-			pw.print(patient.getBirthday() + ";");
-			pw.print(patient.getGender() + ";");
-			pw.print(patient.getAddress() + ";");
-			pw.print(patient.getNumber() + ";");
-			pw.print(patient.getNationalID() + ";");*/
+			 * for (int i = 0; i < this.list.size(); i++) {
+			 * removeDuplicate(this.list);
+			 * pw.print(this.list.get(i) + ";");
+			 * k++;
+			 * if (k == 9) {
+			 * pw.println();
+			 * k = 0;
+			 * }
+			 * }
+			 * 
+			 * pw.print(patient.getUID() + ";");
+			 * pw.print(patient.getFirstName() + ";");
+			 * pw.print(patient.getlastName() + ";");
+			 * pw.print(patient.getMiddleName() + ";");
+			 * pw.print(patient.getBirthday() + ";");
+			 * pw.print(patient.getGender() + ";");
+			 * pw.print(patient.getAddress() + ";");
+			 * pw.print(patient.getNumber() + ";");
+			 * pw.print(patient.getNationalID() + ";");
+			 */
 			pw.print("\n");
 			pw.flush();
 
@@ -82,22 +80,11 @@ public class AddNewPatient {
 		}
 	}
 
-	private void removeDuplicate(ArrayList<String> list2) {
-		ArrayList<String> temp = new ArrayList<>();
-		for (int i = 0; i < this.list.size(); i++) {
+	public boolean isNIDUnique(ArrayList<Patient> patients, String NID) {
 
-			if (!temp.contains(this.list.get(i))) {
-				temp.add(this.list.get(i));
-			}
-		}
-		this.list.clear();
-		this.list.addAll(temp);
-	}
-	public boolean isNIDUnique(ArrayList<Patient> patients, String NID){
-
-		for(int i=0;i<patients.size();i++){
-			//if the National ID already exists in the patient array, return false
-			if(NID.contains(patients.get(i).getNationalID())){
+		for (int i = 0; i < patients.size(); i++) {
+			// if the National ID already exists in the patient array, return false
+			if (NID.contains(patients.get(i).getNationalID())) {
 				return false;
 			}
 		}
@@ -118,36 +105,38 @@ public class AddNewPatient {
 		LocalDateTime now = LocalDateTime.now();
 
 		// Check if UniqueIdentifier is in the text file or used
-		/*String filepath = "Patients.txt";
-		String checkUI = "P202206AAA00";
-
-		boolean found = false;
-		String OldUI, LastName, FirstName, MiddleName,
-				Birthday, Gender, Address, Phone, NI;
-
-		/*try {
-			Scanner x = new Scanner(new File(filepath));
-			x.useDelimiter(";");
-			System.out.println(x);
-
-			while (x.hasNext() && !found) {
-				OldUI = x.next();
-				LastName = x.next();
-				FirstName = x.next();
-				MiddleName = x.next();
-				Birthday = x.next();
-				Gender = x.next();
-				Address = x.next();
-				Phone = x.next();
-				NI = x.next();
-				if (OldUI.equals(checkUI)) {
-					found = true;
-				}
-			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}*/
+		/*
+		 * String filepath = "Patients.txt";
+		 * String checkUI = "P202206AAA00";
+		 * 
+		 * boolean found = false;
+		 * String OldUI, LastName, FirstName, MiddleName,
+		 * Birthday, Gender, Address, Phone, NI;
+		 * 
+		 * /*try {
+		 * Scanner x = new Scanner(new File(filepath));
+		 * x.useDelimiter(";");
+		 * System.out.println(x);
+		 * 
+		 * while (x.hasNext() && !found) {
+		 * OldUI = x.next();
+		 * LastName = x.next();
+		 * FirstName = x.next();
+		 * MiddleName = x.next();
+		 * Birthday = x.next();
+		 * Gender = x.next();
+		 * Address = x.next();
+		 * Phone = x.next();
+		 * NI = x.next();
+		 * if (OldUI.equals(checkUI)) {
+		 * found = true;
+		 * }
+		 * }
+		 * 
+		 * } catch (Exception e) {
+		 * e.printStackTrace();
+		 * }
+		 */
 
 		A = "P";
 		BBBB = yeardtf.format(now);
@@ -164,40 +153,41 @@ public class AddNewPatient {
 		char d2 = lastD.charAt(1);
 		char d3 = lastD.charAt(2);
 
-		//4lastE = 99; // <=== testing lang
+		// 4lastE = 99; // <=== testing lang
 
-		if(lastE == 99){
+		if (lastE == 99) {
 			d3++;
-			if(d3 >= 'Z'){
+			if (d3 >= 'Z') {
 				d2++;
-				if(d2 >= 'Z'){
+				if (d2 >= 'Z') {
 					d1++;
 				}
 			}
 			EE = "01";
-		}
-		else {
-			EE = String.format("%02d", lastE+1);
+		} else {
+			EE = String.format("%02d", lastE + 1);
 		}
 
-		DDD = ""+ d1 + d2 + d3;
-		
+		DDD = "" + d1 + d2 + d3;
+
 		// if Added New Patient
-		/*int i = 0;
-		outerloop: for (char c1 = 'A'; c1 <= 'Z'; c1++)
-			for (char c2 = 'A'; c2 <= 'Z'; c2++)
-				for (char c3 = 'A'; c3 <= 'Z'; c3++)
-					for (int j = 0; j <= 99; j++) {
-						EE = String.format("%02d", j);
-						DDD = "" + c1 + c2 + c3;
-						i++;
-
-						if (i == NewPatientCount)
-							break outerloop;
-					}*/
+		/*
+		 * int i = 0;
+		 * outerloop: for (char c1 = 'A'; c1 <= 'Z'; c1++)
+		 * for (char c2 = 'A'; c2 <= 'Z'; c2++)
+		 * for (char c3 = 'A'; c3 <= 'Z'; c3++)
+		 * for (int j = 0; j <= 99; j++) {
+		 * EE = String.format("%02d", j);
+		 * DDD = "" + c1 + c2 + c3;
+		 * i++;
+		 * 
+		 * if (i == NewPatientCount)
+		 * break outerloop;
+		 * }
+		 */
 
 		UniqueIdentifier = A + BBBB + CC + DDD + EE;
-		System.out.println("unique= " + UniqueIdentifier);	//<===
+		System.out.println("unique= " + UniqueIdentifier); // <===
 		return UniqueIdentifier;
 	}
 
